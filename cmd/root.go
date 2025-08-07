89230ecbd872590171c36802aad216d8c89ffb95
john-1005 <jtgarza48@gmail.com>
2025-08-06 21:10:04 -0700

    Added root.go and first cmd inside

---
+++ cmd/root.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "githubfetcher [username]",
+	Short: "Fetches github respositories for a given user",
+	Long: `githubfetcher is a command-line tool to retreive and display
+				public GitHub respositories for any specified user. It organizes
+				the reposotories in an astethically pleasing table, sorted by
+				the number of stargazers
+
+				Usage Examples:
+				githubfetcher octocat
+				githubfetcher johnsmith --sort asc`,
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			return fmt.Errorf("Github username is required")
+		}
+
+		userName := args[0]
+
+		fmt.Printf("Fetching reposotories for: %s\n", userName)
+
+		return nil
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
